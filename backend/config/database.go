1d70a9e1bf37c440e0d554a19c79e55e043a3b98
LAPTOP-T0M8VSRP\User <syafiqali253@gmail.com>
2024-11-22 13:54:52 +0700

    fixing frontend

--- backend/config/database.go
+++ backend/config/database.go
@@ -13,31 +13,31 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() (*gorm.DB, error) {
-    dsn := os.Getenv("DB_DSN")
-    if dsn == "" {
-        dsn = "host=localhost user=postgres password=admin dbname=pos5 port=5432 sslmode=disable"
-    }
+	dsn := os.Getenv("DB_DSN")
+	if dsn == "" {
+		dsn = "host=localhost user=postgres password=123 dbname=pos5 port=5432 sslmode=disable"
+	}
 
-    database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-    if err != nil {
-        log.Fatal("Failed to connect to database:", err)
-        return nil, err
-    }
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+		return nil, err
+	}
 
-    err = database.AutoMigrate(
-        // &models.User{},
-        // &models.Product{},
-        // &models.Category{},
-        // &models.Sale{},
-        // &models.SalesItem{},
-        // &models.Purchase{},
-        // // &models.Session{},
-    )
-    if err != nil {
-        log.Fatal("Failed to migrate database:", err)
-        return nil, err
-    }
+	err = database.AutoMigrate(
+	// &models.User{},
+	// &models.Product{},
+	// &models.Category{},
+	// &models.Sale{},
+	// &models.SalesItem{},
+	// &models.Purchase{},
+	// // &models.Session{},
+	)
+	if err != nil {
+		log.Fatal("Failed to migrate database:", err)
+		return nil, err
+	}
 
-    DB = database
-    return DB, nil
+	DB = database
+	return DB, nil
 }
