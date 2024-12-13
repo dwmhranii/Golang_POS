88035aadca0ad12ce461a2f53a5f9e5a9f524ce7
dwmhranii <maharanidwi883@gmail.com>
2024-12-13 23:20:38 +0700

    lll

--- backend/main.go
+++ backend/main.go
@@ -11,12 +11,10 @@ import (
 )
 
 func main() {
-	// Load .env file
 	if err := godotenv.Load(".env"); err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	// Connect to Database
 	db, err := config.ConnectDatabase()
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
@@ -24,22 +22,18 @@ func main() {
 
 	app := fiber.New()
 
-	// Enable CORS
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: "*",
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 		AllowMethods: "GET, POST, PUT, DELETE, OPTIONS",
 	}))
 
-	// Share DB instance via Locals
 	app.Use(func(c *fiber.Ctx) error {
 		c.Locals("db", db)
 		return c.Next()
 	})
 
-	// Set up routes
 	routes.Setup(app)
 
-	// Start the server
 	log.Fatal(app.Listen(":3010"))
 }
